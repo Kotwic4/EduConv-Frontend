72b5d2614aa5ec59d5f49f9b6efab5fdbc9c2eca
Dariusz Szymczyk <edarek@op.pl>
2018-05-20 16:06:53 +0200

    Max pooling 2d args added.

--- src/app/network/shared/hidden-layers/hidden-layer/layers/max-pooling2d-layer/max-pooling2d-layer.component.ts
+++ src/app/network/shared/hidden-layers/hidden-layer/layers/max-pooling2d-layer/max-pooling2d-layer.component.ts
@@ -1,15 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output, ViewChild} from '@angular/core';
+import {NgForm} from '@angular/forms';
+import {Store} from '@ngrx/store';
+import * as fromApp from '../../../../../../store/app.reducers';
+import {MaxPooling2DLayerArgs} from './max-pooling2d-layer.model';
 
 @Component({
-  selector: 'app-max-pooling2d-layer',
-  templateUrl: './max-pooling2d-layer.component.html',
-  styleUrls: ['./max-pooling2d-layer.component.scss']
+    selector: 'app-max-pooling2d-layer',
+    templateUrl: './max-pooling2d-layer.component.html',
+    styleUrls: ['./max-pooling2d-layer.component.scss']
 })
 export class MaxPooling2dLayerComponent implements OnInit {
+    @ViewChild('f') confForm: NgForm;
+    @Input() index: number;
+    @Input() layer: any;
+    @Input() readonly;
+    @Output() onSave = new EventEmitter<any>();
+    @Output() onCancel = new EventEmitter<any>();
 
-  constructor() { }
+    constructor(private store: Store<fromApp.AppState>) {}
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+        setTimeout(
+            () => {
+                this.confForm.setValue({
+                    poolX: this.layer.args.pool_size[0],
+                    poolY: this.layer.args.pool_size[1],
+                    strideX: this.layer.args.strides[0],
+                    strideY: this.layer.args.strides[1]
+                });
+            }
+        );
+    }
 
+    onSubmit(form: NgForm) {
+        const args: MaxPooling2DLayerArgs = {
+            pool_size: [form.value.poolX, form.value.poolY],
+            strides: [form.value.strideX, form.value.strideY]
+        };
+
+        this.onSave.emit(args);
+    }
 }
--- src/app/network/shared/hidden-layers/hidden-layer/layers/max-pooling2d-layer/max-pooling2d-layer.model.ts
+++ src/app/network/shared/hidden-layers/hidden-layer/layers/max-pooling2d-layer/max-pooling2d-layer.model.ts
@@ -9,6 +9,15 @@ export class MaxPooling2DLayer extends HiddenLayer {
     layer_name: String = 'MaxPooling2D';
     args: MaxPooling2DLayerArgs;
 
+    constructor() {
+        super();
+
+        this.args = {
+            pool_size: [null, null],
+            strides: [null, null]
+        };
+    }
+
     public setArgs(args: MaxPooling2DLayerArgs) {
         this.args = args;
     }
