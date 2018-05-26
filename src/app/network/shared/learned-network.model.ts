61edc175e18ec451b468b2b7bbfe7ad067f62ce5
Dariusz Szymczyk <edarek@op.pl>
2018-05-26 19:18:29 +0200

    Models refactored.

--- src/app/network/shared/learned-network.model.ts
+++ src/app/network/shared/learned-network.model.ts
@@ -1,5 +1,4 @@
 import * as tf from '@tensorflow/tfjs';
-import {HiddenLayerType} from './hidden-layers/hidden-layer/layers/hidden-layer-type.enum';
 import {HiddenLayersService} from './hidden-layers/hidden-layer/layers/hidden-layer.service';
 import {API_URL} from '../network.consts';
 import {HiddenLayer} from './hidden-layers/hidden-layer/layers/hidden-layer.model';
@@ -12,8 +11,10 @@ export class LearnedNetwork {
     private _input;
     private _model: tf.Sequential;
     private _images = [];
+    private hiddenLayersService: HiddenLayersService;
 
     constructor() {
+        this.hiddenLayersService = new HiddenLayersService();
     }
 
     get id() {
@@ -60,42 +61,14 @@ export class LearnedNetwork {
         return this._images;
     }
 
-    static mapLayerNameToEnum(name): HiddenLayerType {
-        const trueName = name.split('_').slice(0, name.split('_').length - 1).join('');
-        switch (trueName) {
-            case 'conv2d':
-                return HiddenLayerType.Conv2D;
-            case 'maxpooling2d':
-                return HiddenLayerType.MaxPooling2D;
-            case 'dropout':
-                return HiddenLayerType.Dropout;
-            case 'flatten':
-                return HiddenLayerType.Flatten;
-            case 'dense':
-                return HiddenLayerType.Dense;
-        }
-        return HiddenLayerType.Dense;
-    }
-
-    static getLayerInfo(layer) {
-        const type = LearnedNetwork.mapLayerNameToEnum(layer.name);
-        let neurones = 0;
-        if (layer.output instanceof tf.SymbolicTensor) {
-            neurones = layer.output.shape.slice(-1)[0];
-        }
-        const service = new HiddenLayersService();
-        const modalLayer = service.getInstance(type);
-        modalLayer.setNeurons(neurones);
-        modalLayer.setHaveNeurons(true);
-        return modalLayer;
-    }
-
     loadModel() {
         const data = tf.loadModel(API_URL + 'model/' + this._id + '/model.json');
+
         return data.then(
             (model: tf.Sequential) => {
                 this._model = model;
-                this._layers = model.layers.map(LearnedNetwork.getLayerInfo);
+                this._layers = model.layers.map(this._getLayerInfo.bind(this));
+
                 return this;
             }
         );
@@ -110,17 +83,20 @@ export class LearnedNetwork {
         const _min: any = Math.min.apply(null, data);
         const _max: any = Math.max.apply(null, data);
         const conf = (_max - _min) / 255;
+
         if (shape.length === 2) {
             for (let i = 0; i < shape[1]; i++) {
                 const value: any = data[i];
                 const color = (value - _min) / conf;
+
                 canvas.width  = 1;
                 canvas.height = 1;
                 ctx.fillStyle = `rgb(${color},${color},${color})`;
                 ctx.fillRect(0, 0, 1, 1);
                 result.push(canvas.toDataURL());
             }
-        } else {
+        }
+        else {
             const x = shape[1];
             const y = shape[2];
             const z = shape[3];
@@ -128,6 +104,7 @@ export class LearnedNetwork {
             for (let n = 0; n < z; n++) {
                 canvas.width  = x;
                 canvas.height = y;
+
                 for (let i = 0; i < x; i++) {
                     for (let j = 0; j < y; j++) {
                         const index = n + (i*z) + (j*z*x);
@@ -137,28 +114,34 @@ export class LearnedNetwork {
                         ctx.fillRect(i, j, i + 1, j + 1);
                     }
                 }
+
                 result.push(canvas.toDataURL());
             }
         }
+
         return result;
     }
 
     runModel() {
         const img = new Image();
         img.src = this.input;
+
         const images = [];
         const croppedImage = tf.fromPixels(img, 1);
         const batchedImage = croppedImage.expandDims(0).toFloat();
         const reshaped = batchedImage.reshape(this._inputShape);
         let inp: any = reshaped;
         let out: any = reshaped;
+
         for (let i = 0; i < this._model.layers.length; i++) {
             const layer = this._model.getLayer('', i);
             out = layer.apply(inp);
             images.push(this.generateImage(out));
             inp = out;
         }
+
         this._images = images;
+
         return Array.from(out.dataSync());
     }
 
@@ -174,5 +157,25 @@ export class LearnedNetwork {
                 resolve(result);
             }
         );
+    }
+
+    private _getLayerInfo(layer) {
+        const trueName = layer.name.split('_').slice(0, layer.name.split('_').length - 1).join('');
+        const type = this.hiddenLayersService.getTypeByName(trueName);
+
+        if (type === null) {
+            throw new Error("Unrecognized layer type.");
+        }
+
+        let neurones = 0;
+        if (layer.output instanceof tf.SymbolicTensor) {
+            neurones = layer.output.shape.slice(-1)[0];
+        }
+
+        const layerInfo = this.hiddenLayersService.getInstance(type);
+        layerInfo.setNeurons(neurones);
+        layerInfo.setHaveNeurons(true);
+
+        return layerInfo;
     }
 }
