9e93a748d2bf3e29c24293f420451f7f75a8d658
Radomir Krawczykiewicz <kotwic4@gmail.com>
2018-06-19 00:10:23 +0200

    max pooling 1d

---
+++ src/app/network/shared/hidden-layers/hidden-layer/layers/max-pooling1d-layer/max-pooling1d-layer.component.ts
@@ -0,0 +1,42 @@
+import {Component, EventEmitter, Input, OnInit, Output, ViewChild} from '@angular/core';
+import {NgForm} from '@angular/forms';
+import {Store} from '@ngrx/store';
+import * as fromApp from '../../../../../../store/app.reducers';
+import {MaxPooling1DLayerArgs} from './max-pooling1d-layer.model';
+
+@Component({
+    selector: 'app-max-pooling1d-layer',
+    templateUrl: './max-pooling1d-layer.component.html',
+    styleUrls: ['./max-pooling1d-layer.component.scss']
+})
+export class MaxPooling1dLayerComponent implements OnInit {
+    @ViewChild('f') confForm: NgForm;
+    @Input() index: number;
+    @Input() layer: any;
+    @Input() readonly;
+    @Output() onSave = new EventEmitter<any>();
+    @Output() onCancel = new EventEmitter<any>();
+    @Output() onDelete = new EventEmitter<any>();
+
+    constructor(private store: Store<fromApp.AppState>) {}
+
+    ngOnInit() {
+        setTimeout(
+            () => {
+                this.confForm.setValue({
+                    pool: this.layer.args.pool_size,
+                    stride: this.layer.args.strides,
+                });
+            }
+        );
+    }
+
+    onSubmit(form: NgForm) {
+        const args: MaxPooling1DLayerArgs = {
+            pool_size: form.value.pool,
+            strides: form.value.stride,
+        };
+
+        this.onSave.emit(args);
+    }
+}
